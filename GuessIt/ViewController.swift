972ac9221f16ba822a9b54ba182cb3c1702fc507
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-05-30 19:45:10 +0300

    Initial Commit

---
+++ GuessIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuessIt
+//
+//  Created by Ramill Ibragimov on 30.05.2020.
+//  Copyright © 2020 Ramill Ibragimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
